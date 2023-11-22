7f1afecd24adef3ecd10d6c37aa75c6f8662c337
unknown <henri.nigul@voco.ee>
2023-11-22 21:46:59 +0200

    PS - yl5

---
+++ Ylesanne5.ps1
@@ -0,0 +1,11 @@
+﻿Get-Process | ?{$_.ProcessName -eq "notepad"} | Select ProcessName,Id
+
+Second question:
+Get-ChildItem -Path "C:\temp\test"
+$fail = Get-ChildItem -Path "C:\temp\test" | where {$_.Name -like "*.csv"} | Select
+Name,Length
+$KBmaht = $fail.Length/1KB
+$MBmaht = $fail.Length/1MB
+Write-Host "`nFileName : "$fail.Name
+Write-Host "Size in KB : "$KBmaht
+Write-Host "Size in MB : "$MBmaht
