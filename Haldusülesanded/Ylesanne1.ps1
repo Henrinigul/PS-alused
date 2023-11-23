4ba9df430d41a3484537ed7f7a85384f2ba16c99
unknown <henri.nigul@voco.ee>
2023-11-23 10:45:08 +0200

    PS haldusylesanded - yl1

---
+++ Haldusülesanded/Ylesanne1.ps1
@@ -0,0 +1,27 @@
+﻿$eesnimi = Read-Host "Sisesta oma nimi"
+$perenimi = Read-Host "Sisesta oma perenimi"
+
+$kasutajanimi = "$eesnimi.$perenimi"
+
+$taisnimi = "$eesnimi $perenimi"
+
+$teade = "See on kasutaja $taisnimi"
+
+$kasutajanimiVaiketahtedega = $kasutajanimi.ToLower()
+
+$securePassword = ConvertTo-SecureString "Parool1!" -AsPlainText -Force
+
+try {
+    New-LocalUser -Name $kasutajanimiVaiketahtedega -Description $teade -Password $securePassword -ErrorAction Stop
+
+    
+    Set-LocalUser -Name $kasutajanimiVaiketahtedega -PasswordNeverExpires $true
+
+    Write-Host "Kasutaja $kasutajanimi on loodud."
+}
+catch {
+    Write-Host "Viga kasutaja loomisel: $_"
+}
+
+
+
