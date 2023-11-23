718df15b2d6e31d4c7ec2a6cf179d9c30539b564
unknown <henri.nigul@voco.ee>
2023-11-23 11:29:00 +0200

    PS haldusylesanded - yl2

---
+++ Haldusülesanded/Ylesanne2.ps1
@@ -0,0 +1,13 @@
+﻿$eesnimi = Read-Host "Sisesta kasutaja eesnimi"
+$perenimi = Read-Host "Sisesta kasutaja perenimi"
+
+$kasutajanimi = "$eesnimi.$perenimi"
+$taisnimi = "$eesnimi $perenimi"
+
+try {
+    Remove-LocalUser -Name $kasutajanimi -ErrorAction Stop
+    Write-Host "Kasutaja $taisnimi on kustutatud."
+}
+catch {
+    Write-Host "Viga kasutaja kustutamisel: $_"
+}
