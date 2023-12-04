e7e851eba29a9eab55881908bbbc32cb3a4129bf
unknown <Administrator@SV-KOOL.LOCAL>
2023-12-04 22:36:16 +0200

    PS haldusylesanded - yl4

---
+++ Haldusülesanded/Ylesanne4.ps1
@@ -0,0 +1,43 @@
+﻿# Function to translit UTF-8 characters to LATIN
+function Translit {
+    param(
+        [string] $inputString
+    )
+
+    $translit = @{
+        [char]'ä' = "a"
+        [char]'ö' = "o"
+        [char]'ü' = "u"
+        [char]'õ' = "o"
+    }
+
+    $outputString = ""
+
+    foreach ($character in $inputCharacter = $inputString.ToCharArray()) {
+        if ($translit[$character] -ne $null) {
+            $outputString += $translit[$character]
+        } else {
+            $outputString += $character
+        }
+    }
+
+    Write-Output $outputString
+}
+
+# Prompt user for first and last name
+$firstName = Read-Host "Enter the first name of the user"
+$lastName = Read-Host "Enter the last name of the user"
+
+# Create username by combining and transliterating the names
+$username = Translit("$firstName.$lastName").ToLower()
+
+# Display name
+$displayName = "$firstName $lastName"
+
+# Try to remove the user
+try {
+    Remove-ADUser -Identity $username -Confirm:$false
+    Write-Host "User $displayName is removed successfully"
+} catch {
+    Write-Host "User not exists or problem is occuring during user removing, please try again"
+}
